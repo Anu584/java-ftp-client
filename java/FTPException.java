b20a50b314565c3373f70ed3cdb1a1522548625d
Anu584 <82861243+Anu584@users.noreply.github.com>
2021-04-20 11:56:37 +0530

    Update FTPException.java

--- java/FTPException.java
+++ java/FTPException.java
@@ -1,1 +1,13 @@
 
+public class FTPException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FTPException(String message) {
+		super(message);
+	}
+
+}
